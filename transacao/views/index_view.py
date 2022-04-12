94b60ead2a47891fdcdc5275db3bec237be25a7e
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-04-11 20:33:45 -0500

    change index_view.py save file upload on database

--- transacao/views/index_view.py
+++ transacao/views/index_view.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from ..forms.arquivo_model_form import ArquivoForm
 
 
 def index(request):
-    form = ArquivoForm()
+    if request.method == 'POST':
+        form = ArquivoForm(request.POST, request.FILES)
+        if form.is_valid():
+            print(request.FILES['arquivo'].name)
+            print(request.FILES['arquivo'].size)
+            form.save()
+            return redirect('transacao:index')
+    else:
+        form = ArquivoForm()
     return render(request, 'transacao/index.html', {'form': form})
