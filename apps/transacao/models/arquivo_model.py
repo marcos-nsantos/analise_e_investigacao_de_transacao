3dee6fb44c0b50b41e7bde1b3a06f2e6d5e37c1d
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-04-12 10:11:59 -0500

    add validator to check if file uploaded is empty

--- apps/transacao/models/arquivo_model.py
+++ apps/transacao/models/arquivo_model.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from ..validators.csv_file import is_csv_file
+from ..validators.csv_file import is_csv_file, is_file_empty
 
 
 class Arquivo(models.Model):
-    arquivo = models.FileField(_('Arquivo'), upload_to='arquivos_csv/', null=True, blank=True, validators=[is_csv_file])
+    arquivo = models.FileField(_('Arquivo'), upload_to='arquivos_csv/', null=True, blank=True, validators=[is_csv_file,
+                                                                                                           is_file_empty])
     created_at = models.DateTimeField(_('Criado em'), auto_now_add=True)
     updated_at = models.DateTimeField(_('Atualizado em'), auto_now=True)
 
--- apps/transacao/validators/csv_file.py
+++ apps/transacao/validators/csv_file.py
@@ -5,3 +5,8 @@ from django.utils.translation import gettext_lazy as _
 def is_csv_file(value):
     if not value.name.endswith('.csv'):
         raise ValidationError(_('O arquivo deve ser um arquivo CSV.'))
+
+
+def is_file_empty(value):
+    if value.size == 0:
+        raise ValidationError(_('O arquivo está vazio.'))
