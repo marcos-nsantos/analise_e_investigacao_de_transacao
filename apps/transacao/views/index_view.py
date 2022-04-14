d6e3fcb5281c87880ebc1e401ccf11619b43d916
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-04-14 12:06:36 -0500

    fix validation when transaction already exists with same date

--- apps/transacao/views/index_view.py
+++ apps/transacao/views/index_view.py
@@ -1,9 +1,10 @@
 import csv
 from datetime import datetime
 
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.db import transaction
 from django.core.exceptions import ValidationError
+from django.contrib import messages
 
 from ..forms.arquivo_model_form import ArquivoForm
 from ..models.transacao_model import Transacao
@@ -14,39 +15,47 @@ def index(request):
         form = ArquivoForm(request.POST, request.FILES)
         if form.is_valid():
             csv_file = request.FILES['arquivo'].read().decode('utf-8')
-            data_hora = capture_date_time_from_csv_file(csv_file)
+            data_hora_primeira_linha = capture_date_time_from_csv_file(csv_file)
 
-            try:
-                with transaction.atomic():
-                    instance = form.save(commit=False)
-                    instance.save()
+            error_messages = []
+            with transaction.atomic():
+                instance = form.save(commit=False)
+                instance.save()
 
-                    for row in csv.reader(csv_file.splitlines(), delimiter=','):
-                        try:
-                            banco_origem = row[0]
-                            agencia_origem = row[1]
-                            conta_origem = row[2]
-                            banco_destino = row[3]
-                            agencia_destino = row[4]
-                            conta_destino = row[5]
-                            valor = float(row[6])
+                for row in csv.reader(csv_file.splitlines(), delimiter=','):
+                    try:
+                        banco_origem = row[0]
+                        agencia_origem = row[1]
+                        conta_origem = row[2]
+                        banco_destino = row[3]
+                        agencia_destino = row[4]
+                        conta_destino = row[5]
+                        valor = float(row[6])
+                        data_hora = datetime.strptime(row[7], '%Y-%m-%dT%H:%M:%S')
 
-                            if not transaction_already_exists(banco_origem, agencia_origem, conta_origem, banco_destino,
-                                                              agencia_destino, conta_destino, valor):
-                                transacao = Transacao(arquivo=instance, data_hora=data_hora, banco_origem=banco_origem,
-                                                      agencia_origem=agencia_origem, conta_origem=conta_origem,
-                                                      banco_destino=banco_destino, agencia_destino=agencia_destino,
-                                                      conta_destino=conta_destino, valor=valor)
-                                transacao.full_clean()
-                                transacao.save()
-                        except ValidationError:
-                            pass
-                        except ValueError:
-                            pass
-            except ValidationError:
-                pass
+                        if data_hora_primeira_linha.date() == data_hora.date() and not transaction_already_exists(
+                                banco_origem, agencia_origem, conta_origem, banco_destino, agencia_destino,
+                                conta_destino, data_hora, valor):
+                            transacao = Transacao(arquivo=instance, data_hora=data_hora_primeira_linha,
+                                                  banco_origem=banco_origem,
+                                                  agencia_origem=agencia_origem, conta_origem=conta_origem,
+                                                  banco_destino=banco_destino, agencia_destino=agencia_destino,
+                                                  conta_destino=conta_destino, valor=valor)
+                            transacao.full_clean()
+                            transacao.save()
+                        elif transaction_already_exists(banco_origem, agencia_origem, conta_origem, banco_destino,
+                                                        agencia_destino, conta_destino, data_hora, valor):
+                            error_messages.append(f'Não foi possível salvar a transação {row}. Pois esta já existe em '
+                                                  f'nossa base de dados.')
+                    except ValueError:
+                        pass
+                    except ValidationError:
+                        pass
 
-            return redirect('transacao:index')
+            if error_messages:
+                for error_message in error_messages:
+                    messages.error(request, error_message)
+
     else:
         form = ArquivoForm()
     return render(request, 'transacao/index.html', {'form': form})
@@ -59,10 +68,11 @@ def capture_date_time_from_csv_file(arquivo_csv):
 
 
 def transaction_already_exists(banco_origem, agencia_origem, conta_origem, banco_destino, agencia_destino,
-                               conta_destino, valor):
+                               conta_destino, data_hora, valor):
     is_transaction = Transacao.objects.filter(banco_origem=banco_origem, agencia_origem=agencia_origem,
                                               conta_origem=conta_origem,
                                               banco_destino=banco_destino, agencia_destino=agencia_destino,
-                                              conta_destino=conta_destino,
+                                              conta_destino=conta_destino, data_hora__year=data_hora.year,
+                                              data_hora__month=data_hora.month, data_hora__day=data_hora.day,
                                               valor=valor).exists()
     return is_transaction
