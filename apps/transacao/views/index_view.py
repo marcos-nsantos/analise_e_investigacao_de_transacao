561733de409d0b162b6a51a00e588c2b97d46531
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-04-14 12:34:09 -0500

    add success message when file is uploaded

--- apps/transacao/views/index_view.py
+++ apps/transacao/views/index_view.py
@@ -18,39 +18,44 @@ def index(request):
             data_hora_primeira_linha = capture_date_time_from_csv_file(csv_file)
 
             error_messages = []
-            with transaction.atomic():
-                instance = form.save(commit=False)
-                instance.save()
+            try:
+                with transaction.atomic():
+                    instance = form.save(commit=False)
+                    instance.save()
 
-                for row in csv.reader(csv_file.splitlines(), delimiter=','):
-                    try:
-                        banco_origem = row[0]
-                        agencia_origem = row[1]
-                        conta_origem = row[2]
-                        banco_destino = row[3]
-                        agencia_destino = row[4]
-                        conta_destino = row[5]
-                        valor = float(row[6])
-                        data_hora = datetime.strptime(row[7], '%Y-%m-%dT%H:%M:%S')
+                    for row in csv.reader(csv_file.splitlines(), delimiter=','):
+                        try:
+                            banco_origem = row[0]
+                            agencia_origem = row[1]
+                            conta_origem = row[2]
+                            banco_destino = row[3]
+                            agencia_destino = row[4]
+                            conta_destino = row[5]
+                            valor = float(row[6])
+                            data_hora = datetime.strptime(row[7], '%Y-%m-%dT%H:%M:%S')
 
-                        if data_hora_primeira_linha.date() == data_hora.date() and not transaction_already_exists(
-                                banco_origem, agencia_origem, conta_origem, banco_destino, agencia_destino,
-                                conta_destino, data_hora, valor):
-                            transacao = Transacao(arquivo=instance, data_hora=data_hora_primeira_linha,
-                                                  banco_origem=banco_origem,
-                                                  agencia_origem=agencia_origem, conta_origem=conta_origem,
-                                                  banco_destino=banco_destino, agencia_destino=agencia_destino,
-                                                  conta_destino=conta_destino, valor=valor)
-                            transacao.full_clean()
-                            transacao.save()
-                        elif transaction_already_exists(banco_origem, agencia_origem, conta_origem, banco_destino,
-                                                        agencia_destino, conta_destino, data_hora, valor):
-                            error_messages.append(f'Não foi possível salvar a transação {row}. Pois esta já existe em '
-                                                  f'nossa base de dados.')
-                    except ValueError:
-                        pass
-                    except ValidationError:
-                        pass
+                            if data_hora_primeira_linha.date() == data_hora.date() and not transaction_already_exists(
+                                    banco_origem, agencia_origem, conta_origem, banco_destino, agencia_destino,
+                                    conta_destino, data_hora, valor):
+                                transacao = Transacao(arquivo=instance, data_hora=data_hora_primeira_linha,
+                                                      banco_origem=banco_origem,
+                                                      agencia_origem=agencia_origem, conta_origem=conta_origem,
+                                                      banco_destino=banco_destino, agencia_destino=agencia_destino,
+                                                      conta_destino=conta_destino, valor=valor)
+                                transacao.full_clean()
+                                transacao.save()
+                            elif transaction_already_exists(banco_origem, agencia_origem, conta_origem, banco_destino,
+                                                            agencia_destino, conta_destino, data_hora, valor):
+                                error_messages.append(f'Não foi possível salvar a transação {row}. Pois esta já existe '
+                                                      f'em nossa base de dados.')
+                        except ValueError:
+                            pass
+                        except ValidationError:
+                            pass
+            except Exception as e:
+                print(e)
+            else:
+                messages.success(request, 'Arquivo importado com sucesso!')
 
             if error_messages:
                 for error_message in error_messages:
