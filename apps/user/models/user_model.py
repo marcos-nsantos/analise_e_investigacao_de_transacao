90c9359442a02e61ebe88491457e8f3070f1f26b
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2022-04-21 23:26:27 -0500

    first name and last name required

--- apps/user/models/user_model.py
+++ apps/user/models/user_model.py
@@ -7,6 +7,8 @@ from .user_manager import UserManager
 
 class User(AbstractUser):
     username = None
+    first_name = models.CharField(_('first name'), max_length=150, blank=False, null=False)
+    last_name = models.CharField(_('last name'), max_length=150, blank=False, null=False)
     email = models.EmailField(_('email address'), unique=True, blank=False, null=False)
 
     USERNAME_FIELD = 'email'
